a5b94b4a85a0adb5f018b40ff4888c652f7b7fc0
syhhw <syhhalves@gmail.com>
2021-08-04 13:20:13 +0000

    vayu: Compile with Proton Clang
    Signed-off-by: syhhw <syhhalves@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,8 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_vayu
 
 # Kernel
 TARGET_KERNEL_CONFIG := vayu_user_defconfig
+TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 
 # Inherit from the proprietary version
 include vendor/xiaomi/vayu/BoardConfigVendor.mk
