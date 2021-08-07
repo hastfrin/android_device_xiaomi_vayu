40d336b9944f0d069d94ae42d5ef1a541dcdbdcf
hastfrin <eusyhhalves@gmail.com>
2021-08-07 16:10:34 -0300

    vayu: PixelBlaster Bringup
    Signed-off-by: hastfrin <eusyhhalves@gmail.com>

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -8,17 +8,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelBlaster stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-PRODUCT_NAME := lineage_vayu
+PRODUCT_NAME := aosp_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO X3 Pro
+PRODUCT_MODEL := Poco X3 Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
@@ -26,3 +31,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vayu_global-user 11 RKQ1.200826.002 V12.5.2.0.RJUMIXM release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:11/RKQ1.200826.002/V12.5.2.0.RJUMIXM:user/release-keys
+
+# Blaster Official
+BLASTER_BUILD_TYPE := OFFICIAL
+
