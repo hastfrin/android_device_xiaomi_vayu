c0eb3079104861506efd0d34ae5b0d515ae95740
hastfrin <eusyhhalves@gmail.com>
2021-08-07 16:59:59 -0300

    vayu: Changed kernel target config
    Signed-off-by: hastfrin <eusyhhalves@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_vayu
 TARGET_RECOVERY_DEVICE_MODULES := libinit_vayu
 
 # Kernel
-TARGET_KERNEL_CONFIG := vendor/vayu_defconfig
+TARGET_KERNEL_CONFIG := vayu_user_defconfig
 
 # Inherit from the proprietary version
 include vendor/xiaomi/vayu/BoardConfigVendor.mk
